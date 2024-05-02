b8352f49bb01d212e7655bae9bbcf6f9b9b5e41f
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-05-01 23:43:47 -0400

    chore:comment

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	// Really hate returning the state and settings to put into loop
+	// Really hate returning the state and settings pointers to put into loop
 	// but it seems like the only way to avoid global state
 	state, settings := PomodoroInit()
 
